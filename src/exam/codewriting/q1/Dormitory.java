360d57432eb60d798a3deeb0837b64c567996b13
Karl Lyu <k.lyu@wustl.edu>
2026-04-19 18:21:29 -0500

    First

--- src/exam/codewriting/q1/Dormitory.java
+++ src/exam/codewriting/q1/Dormitory.java
@@ -8,7 +8,8 @@ import java.util.Map;
 
 		name
 		Mapping of room numbers (like 204H) to Students, For the
-		purposes of this problem, you can assume that each room can only contain one student.
+		purposes of this problem, you can assume that
+		each room can only contain one student.
  */
 public class Dormitory
 {
@@ -17,32 +18,37 @@ public class Dormitory
 	private HashMap < String , Student > rooms ;
 	// Write the constructor for the Dormitory class:
 	// (For full credit the constructor should use only one parameter)
+	public Dormitory(String name){
+		this.name = name;
+		this.rooms = new HashMap<>();
+	}
 
 
 	//Now, write out the remaining methods:
 	public String getName () {
-		return ""; //FIXME
+		return name;
 	}
 
 	/**
 	 * Returns the amount of rooms that are currently occupied.
 	 */
 	public int getOccupiedRooms () {
-		return 0;//FIXME
+		return rooms.size();
 	}
 	
 	/**
 	* Sets the given room number to be occupied by the given Student.
 	*/
-	public void addStudent ( String roomNo , Student s) {
-		//FIXME
+	public void addStudent (String roomNo , Student s) {
+		rooms.put(roomNo, s);
 	}
 	
 	/**
 	* Returns true if the given room contains a Student, false otherwise
 	*/
 	public boolean isOccupied ( String roomNo ) {
-		return false; //FIXME
+		// getter returns null when room is unoccupied
+		return (rooms.get(roomNo) != null);
 	}
 	
 	/**
@@ -51,15 +57,27 @@ public class Dormitory
 	* @return A List containing the student ID numbers of all students in this dorm
 	*/
 	public LinkedList < Integer > getStudentIds () {
-		return null; //FIXME
+		LinkedList<Integer> resident_id = new LinkedList<>();
+		for (Student student : rooms.values()){
+			resident_id.add(student.getId());
+		}
+		return resident_id;
 	}
 	
 	public static void main(String[] args) {
 		
 		//Create an instance of the Dormitory class and store it in a variable called dorm.
-		
+		Dormitory dorm = new Dormitory("Umrath");
+
 		//Create an instance of Student and add it to the dorm. You can choose which room to add it to.
+		Student student1 = new Student(518);
+		Student student2 = new Student(382);
+		//In object-oriented programming we always use instance methods to change attributes
+		dorm.addStudent("201E", student1);
+		dorm.addStudent("201W", student2);
+
 		
 		//Write code to determine if a student with the id of 123456 is in your dorm using the appropriate methods.
+		System.out.println(dorm.getStudentIds());
 	}
 }
